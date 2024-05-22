9fcbbf2b0f5a05b67301dcef5435e9d3525e2463
AARON SALAZAR MATA <aaron.salazar.mata@gmail.com>
2024-05-21 22:00:58 -0600

    cambios

---
+++ Vista/LogIn.java
@@ -0,0 +1,18 @@
+package Vista;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class LogIn extends JFrame {
+
+    public JPanel panelLogIn = new JPanel();
+    public JLabel usuario, contrasena, mensaje;
+    
+
+    public LogIn (){
+
+
+
+    }
+}

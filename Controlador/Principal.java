f3f61a2f14e05b6a25beefdfc32b83d54368a578
AARON SALAZAR MATA <aaron.salazar.mata@gmail.com>
2024-05-26 22:39:29 -0600

    Documentacion de Modelo

--- Controlador/Principal.java
+++ Controlador/Principal.java
@@ -11,10 +11,10 @@ import Vista.VistaPrincipal;
 public class Principal {
 
     public static void main(String[] args) {
-        
 
-        //Se crea una instancia de las respectivas clases que han sido heredadas con JFrame implementando la 
-        //libreria javax.swing.*;
+        // Se crea una instancia de las respectivas clases que han sido heredadas con
+        // JFrame implementando la
+        // libreria javax.swing.*;
         VistaPrincipal vistaPrincipal = new VistaPrincipal();
         LogIn logIn = new LogIn();
         BuzonClientes buzonClientes = new BuzonClientes();
@@ -23,12 +23,13 @@ public class Principal {
         Solicitudes solicitudes = new Solicitudes();
         Pedidos pedidos = new Pedidos();
 
-        //Se crea una instancia de la Clase Metdos en donde se le pasan como paramatreos las instancias de las clases anteriormente mencioandas
+        // Se crea una instancia de la Clase Metdos en donde se le pasan como
+        // paramatreos las instancias de las clases anteriormente mencionadas
         Metodos metodos = new Metodos(vistaPrincipal, logIn, buzonClientes, estado, noticias, solicitudes, pedidos);
 
-        
-        
-        //Para cada instancia se le incova el metodo se "setMetodos, el cual es un metodo constructor para poder trabajar con la clase metodos en todas las clases instanciadas anteriormente"
+        // Para cada instancia se le incova el metodo se "setMetodos", el cual es un
+        // metodo constructor para poder trabajar con la clase metodos en todas las
+        // clases instanciadas anteriormente
         vistaPrincipal.setMetodos(metodos);
         logIn.setMetodos(metodos);
         buzonClientes.setMetodos(metodos);
@@ -37,11 +38,9 @@ public class Principal {
         solicitudes.setMetodos(metodos);
         pedidos.setMetodos(metodos);
 
-
+        // Se hace visible la clase LogIn que es donde el usuario inicia sesion
         logIn.setVisible(true);
-       
-    }
 
-    
+    }
 
 }
